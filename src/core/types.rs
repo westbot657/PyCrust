637b3626f07985f3990ba7f88ef8bd27d777a8f9
westbot <82739021+westbot657@users.noreply.github.com>
2025-12-21 23:40:49 -0700

    working on manual lexer cuz f-strings

--- src/core/types.rs
+++ src/core/types.rs
@@ -1,25 +1,167 @@
 
 
+#[cfg_attr(feature = "serial", derive(serde::Serialize, serde::Deserialize))]
 pub struct TextPosition {
-    index: usize,
-    column: usize,
-    row: usize,
+    pub index: usize,
+    pub column: usize,
+    pub row: usize,
 }
 
+#[cfg_attr(feature = "serial", derive(serde::Serialize, serde::Deserialize))]
 pub struct TextSpan {
     start: TextPosition,
     end: TextPosition,
 }
 
+#[cfg_attr(feature = "serial", derive(serde::Serialize, serde::Deserialize), serde(rename_all = "snake_case"))]
+pub enum Keyword {
+    Return,
+    Continue,
+    Break,
+    If,
+    Elif,
+    Else,
+    While,
+    For,
+    In,
+    Is,
+    As,
+    With,
+    Yield,
+    Not,
+    And,
+    Or,
+    Class,
+    Def,
+    Try,
+    Except,
+    Finally,
+    Import,
+    From,
+}
+
+#[cfg_attr(feature = "serial", derive(serde::Serialize, serde::Deserialize))]
+pub enum Symbol {
+    #[cfg_attr(feature = "serial", serde(rename="("))]
+    LParen,
+    #[cfg_attr(feature = "serial", serde(rename=")"))]
+    RParen,
+    #[cfg_attr(feature = "serial", serde(rename="["))]
+    LBracket,
+    #[cfg_attr(feature = "serial", serde(rename="]"))]
+    RBracket,
+    #[cfg_attr(feature = "serial", serde(rename="{"))]
+    LBrace,
+    #[cfg_attr(feature = "serial", serde(rename="}"))]
+    RBrace,
+    #[cfg_attr(feature = "serial", serde(rename="->"))]
+    Arrow,
+    #[cfg_attr(feature = "serial", serde(rename="@"))]
+    Decorator,
+    #[cfg_attr(feature = "serial", serde(rename="="))]
+    Assign,
+    #[cfg_attr(feature = "serial", serde(rename="."))]
+    Dot,
+    #[cfg_attr(feature = "serial", serde(rename=","))]
+    Comma,
+    #[cfg_attr(feature = "serial", serde(rename=":"))]
+    Colon,
+}
+
+#[cfg_attr(feature = "serial", derive(serde::Serialize, serde::Deserialize))]
+pub enum Comparator {
+    #[cfg_attr(feature = "serial", serde(rename="=="))]
+    Eq,
+    #[cfg_attr(feature = "serial", serde(rename="!="))]
+    Ne,
+    #[cfg_attr(feature = "serial", serde(rename="<"))]
+    Lt,
+    #[cfg_attr(feature = "serial", serde(rename=">"))]
+    Gt,
+    #[cfg_attr(feature = "serial", serde(rename="<="))]
+    Le,
+    #[cfg_attr(feature = "serial", serde(rename=">="))]
+    Ge,
+}
+
+#[cfg_attr(feature = "serial", derive(serde::Serialize, serde::Deserialize))]
+pub enum Operator {
+    #[cfg_attr(feature = "serial", serde(rename="+"))]
+    Add,
+    #[cfg_attr(feature = "serial", serde(rename="-"))]
+    Sub,
+    #[cfg_attr(feature = "serial", serde(rename="*"))]
+    Mul,
+    #[cfg_attr(feature = "serial", serde(rename="/"))]
+    Div,
+    #[cfg_attr(feature = "serial", serde(rename="**"))]
+    Pow,
+    #[cfg_attr(feature = "serial", serde(rename="%"))]
+    Mod,
+    #[cfg_attr(feature = "serial", serde(rename="//"))]
+    Floor,
+    #[cfg_attr(feature = "serial", serde(rename="<<"))]
+    Lsh,
+    #[cfg_attr(feature = "serial", serde(rename=">>"))]
+    Rsh,
+    #[cfg_attr(feature = "serial", serde(rename="|"))]
+    BitOr,
+    #[cfg_attr(feature = "serial", serde(rename="&"))]
+    BitAnd,
+    #[cfg_attr(feature = "serial", serde(rename="^"))]
+    BitXor,
+    #[cfg_attr(feature = "serial", serde(rename="~"))]
+    BitNot,
+}
+
+#[cfg_attr(feature = "serial", derive(serde::Serialize, serde::Deserialize), serde(rename_all = "snake_case"))]
+pub enum Number {
+    I64(i64),
+    F64(f64),
+}
+
+#[cfg_attr(feature = "serial", derive(serde::Serialize, serde::Deserialize), serde(rename_all = "snake_case"))]
+pub enum PartialFString {
+    StringContent(String),
+    TokenStream(Vec<Token>)
+}
+
+#[cfg_attr(feature = "serial", derive(serde::Serialize, serde::Deserialize), serde(rename_all = "snake_case"))]
+pub enum TokenValue {
+    Symbol(Symbol),
+    Keyword(Keyword),
+    Word(String),
+    Operator(Operator),
+    AssignOperator(Operator),
+    Comparator(Comparator),
+    StringLiteral(String),
+    BooleanLiteral(bool),
+    NumberLiteral(Number),
+    Comment(String),
+    DocComment(String),
+    FString(Vec<PartialFString>),
+    Newline,
+    LeadingWhitespace,
+    Indent,
+    Dedent,
+}
+
+#[cfg_attr(feature = "serial", derive(serde::Serialize, serde::Deserialize))]
 pub struct Token {
+    value: TokenValue,
     span: TextSpan,
+    content: String,
 }
 
-pub struct TokenRef<'t> {
-    token: &'t Token,
-    content: &'t str
+impl TextPosition {
+    pub fn zero() -> Self {
+        Self {
+            index: 0,
+            column: 0,
+            row: 0,
+        }
+    }
 }
-
 
 
 
--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -1,15 +1,49 @@
 use crate::core::types::{TextPosition, Token};
+use anyhow::Result;
 
 pub struct Lexer {
     source: String,
     position: TextPosition,
+    tokens: Vec<Token>,
 }
 
+pub enum StringDelimiter {
+    F1x3, // '''
+    F2x3, // """
+    F1x1, // '
+    F2x1, // "
+}
 
+impl Lexer {
 
+    pub fn new(source: String) -> Self {
+        Self {
+            source,
+            position: TextPosition::zero(),
+            tokens: Vec::new(),
+        }
+    }
 
+    pub fn tokenize(&mut self) -> Result<()> {
+        let source = self.source.clone();
+        let length = source.len();
 
+        while self.position.index < length {
+            let remaining = &source[self.position.index..];
+            if let Some(after) = remaining.strip_prefix("f'''") {
+                let token = self.lex_f_string(StringDelimiter::F1x3, after)?;
+            }
+            else if let Some(after) = remaining.strip_prefix("f\"\"\"") {
+                let token = self.lex_f_string(StringDelimiter::F2x3, after)?;
+            }
+        }
 
+        Ok(())
+    }
 
+    fn lex_f_string(&mut self, delimiter: StringDelimiter, current: &str) -> Result<(Vec<Token>, &str)> {
+        todo!()
+    }
 
+}
 
